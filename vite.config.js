ad709914f74e66ed5f5df3a97cc26f4badf05e14
MdAbullah1212 <mda248256@gamil.com>
2026-03-24 07:26:58 +0600

    added base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
+  base: '/mission_restart_a3_assignment/',
 })
